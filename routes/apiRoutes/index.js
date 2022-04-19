a3ec482f5cb6e0dba0de093db1d3fe0436ae24ff
Jordan <jordan.m.edginton@gmail.com>
2022-04-18 18:18:25 -0600

    added delete route

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -22,4 +22,19 @@ router.post("/api/notes",function(req,res){
     res.json(dbJson)
 });
 
+router.delete("/api/notes/:id",function(req,res){
+   //let updatedNotsList = dbJson.forEach(Element => Element.id !== req.params.id)
+   let updatedNotsList = []
+   for(let i = 0; i < dbJson.length; i++) {
+       if (req.params.id != dbJson[i].id) {
+           updatedNotsList.push(dbJson[i])
+       }
+   }
+   console.log(updatedNotsList)
+  dbJson = updatedNotsList
+  fs.writeFileSync("./db/db.json",JSON.stringify(dbJson),function(err){
+      if(err) throw err;
+  })
+    console.log("POST Route",dbJson)
+
 module.exports = router;
