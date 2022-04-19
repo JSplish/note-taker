8c600c3349b1b4f6ac7824219de381bc4e2ea511
Jordan <jordan.m.edginton@gmail.com>
2022-04-18 18:37:40 -0600

    able to add and delete notes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,40 +1,43 @@
 const router = require('express').Router();
-const dbJson = require("../../db/db.json");
+let dbJson = require("../../db/db.json");
 const fs = require("fs");
 
-router.get("/api/notes",function(req,res){
-    dbJson = JSON.parse(fs.readFileSync("./db/db.json","utf-8")) || []
-    console.log("Get Route",dbJson)
+router.get("/api/notes", function (req, res) {
+    dbJson = JSON.parse(fs.readFileSync("./db/db.json", "utf-8")) || []
+    console.log("Get Route", dbJson)
     res.json(dbJson)
 });
 
-router.post("/api/notes",function(req,res){
-    let  userNode = {
+router.post("/api/notes", function (req, res) {
+    let userNode = {
         id: Math.floor(Math.random() * 100),
         title: req.body.title,
         text: req.body.text
     }
+    console.log(userNode)
     dbJson.push(userNode)
-  fs.writeFileSync("./db/db.json",JSON.stringify(dbJson),function(err){
-      if(err) throw err;
-  })
-    console.log("POST Route",dbJson)
+    fs.writeFileSync("./db/db.json", JSON.stringify(dbJson), function (err) {
+        if (err) throw err;
+    })
+    console.log("POST Route", dbJson)
     res.json(dbJson)
 });
 
-router.delete("/api/notes/:id",function(req,res){
-   //let updatedNotsList = dbJson.forEach(Element => Element.id !== req.params.id)
-   let updatedNotsList = []
-   for(let i = 0; i < dbJson.length; i++) {
-       if (req.params.id != dbJson[i].id) {
-           updatedNotsList.push(dbJson[i])
-       }
-   }
-   console.log(updatedNotsList)
-  dbJson = updatedNotsList
-  fs.writeFileSync("./db/db.json",JSON.stringify(dbJson),function(err){
-      if(err) throw err;
-  })
-    console.log("POST Route",dbJson)
+router.delete("/api/notes/:id", function (req, res) {
+    //let updatedNotsList = dbJson.forEach(Element => Element.id !== req.params.id)
+    let updatedNotsList = []
+    for (let i = 0; i < dbJson.length; i++) {
+        if (req.params.id != dbJson[i].id) {
+            updatedNotsList.push(dbJson[i])
+        }
+    }
+    console.log(updatedNotsList)
+    dbJson = updatedNotsList
+    fs.writeFileSync("./db/db.json", JSON.stringify(dbJson), function (err) {
+        if (err) throw err;
+    })
+    console.log("Delete Route", dbJson)
+    res.json(dbJson)
+})
 
 module.exports = router;
