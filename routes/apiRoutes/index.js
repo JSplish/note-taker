b370458e006adc85ecf21871bf8bcf68da91cb36
Jordan <jordan.m.edginton@gmail.com>
2022-04-18 18:12:22 -0600

    added get and post routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,25 @@
 const router = require('express').Router();
-const noteRoutes = require('./noteRoutes');
+const dbJson = require("../../db/db.json");
+const fs = require("fs");
 
-router.use(noteRoutes);
+router.get("/api/notes",function(req,res){
+    dbJson = JSON.parse(fs.readFileSync("./db/db.json","utf-8")) || []
+    console.log("Get Route",dbJson)
+    res.json(dbJson)
+});
+
+router.post("/api/notes",function(req,res){
+    let  userNode = {
+        id: Math.floor(Math.random() * 100),
+        title: req.body.title,
+        text: req.body.text
+    }
+    dbJson.push(userNode)
+  fs.writeFileSync("./db/db.json",JSON.stringify(dbJson),function(err){
+      if(err) throw err;
+  })
+    console.log("POST Route",dbJson)
+    res.json(dbJson)
+});
 
 module.exports = router;
