54bb0d81ae202978ae3f2d35e6bc980d34d7e9d6
Jordan <jordan.m.edginton@gmail.com>
2022-04-18 18:02:06 -0600

    add variables to server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+app.use(express.static('public'));
+app.use(require('./routes/apiRoutes'));
+app.use(require('./routes/htmlRoutes'));
+
+app.listen(PORT,function(){
+    console.log('Listening');
+})
